1e4e8de7a55f1767be7ae7338fc234a2529e7eed
Marko Živanović <zivanovic.marko@icloud.com>
2025-01-16 17:02:07 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Marko Zivanovic on 16.1.25..
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
